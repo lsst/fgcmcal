fffe14cf218d9400b8aa7053241ae7056380ad11
Eli Rykoff <erykoff@slac.stanford.edu>
2017-10-03 13:52:34 -0700

    Fix up linting

--- python/lsst/fgcm/__init__.py
+++ python/lsst/fgcm/__init__.py
@@ -2,7 +2,8 @@
 # See COPYRIGHT file at the top of the source tree
 #
 
-import pkgutil, lsstimport
+import pkgutil
+import lsstimport
 
 from .fgcmFitCycle import *
 from .fgcmBuildStars import *
@@ -10,6 +11,3 @@ from .fgcmMakeLut import *
 from .detectorThroughput import *
 
 __path__ = pkgutil.extend_path(__path__, __name__)
-
-#from detectorThroughput import DetectorThroughput
-
