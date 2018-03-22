e0aa02cf5c4c6bdbf233564f75bdfcbf29fddf5e
Eli Rykoff <erykoff@stanford.edu>
2018-03-22 12:48:28 -0700

    Add first tests using testdata_jointcal

--- tests/fgcmcalTestBase.py
+++ tests/fgcmcalTestBase.py
@@ -4,8 +4,13 @@ from __future__ import division, absolute_import, print_function
 
 import os
 import inspect
+import shutil
+import numpy as np
 
-from lsst.fgcmcal import fgcmcal
+import lsst.daf.persistence as dafPersistence
+import lsst.log
+
+import lsst.fgcmcal as fgcmcal
 
 class FgcmcalTestBase(object):
     """
@@ -14,7 +19,7 @@ class FgcmcalTestBase(object):
     Derive from this first, then from TestCase.
     """
 
-    def setUp_base(self):
+    def setUp_base(self, inputDir=None, testDir=None, logLevel=None, otherArgs=[]):
         """
         Call from your child class's setUp() to get variables built.
 
@@ -25,43 +30,123 @@ class FgcmcalTestBase(object):
         """
 
         self.config = None
-        self.inputDir = None
-        self.logLevel = None
+        self.inputDir = inputDir
+        self.testDir = testDir
+        self.logLevel = logLevel
+        self.otherArgs = otherArgs
+
+        lsst.log.setLevel("daf.persistence.butler", lsst.log.FATAL)
+        lsst.log.setLevel("CameraMapper", lsst.log.FATAL)
 
     def tearDown(self):
         if getattr(self, 'config', None) is not None:
             del self.config
 
-    def _runFgcmMakeLut(self, outputDir):
+    def _runFgcmMakeLut(self, nBand, i0Std, i0Recon, i10Std, i10Recon):
         """
         """
 
         if self.logLevel is not None:
             self.otherArgs.extend(['--loglevel', 'fgcmcal=%s'%self.logLevel])
-        args = [self.inputDir, '--output', outputDir,
+        args = [self.inputDir, '--output', self.testDir,
                 '--doraise']
         args.extend(self.otherArgs)
 
-        result = fgcmcal.fgcmMakeLutTask.parseAndRun(args=args, config=self.config)
+        result = fgcmcal.FgcmMakeLutTask.parseAndRun(args=args, config=self.config)
         self._checkResult(result)
 
-    def _runFgcmBuildStars(self, outputDir):
+        butler = dafPersistence.butler.Butler(self.testDir)
+        tempTask = fgcmcal.FgcmFitCycleTask()
+        fgcmLut, lutIndexVals, lutStd = tempTask._loadFgcmLut(butler)
+
+        # Check that we got the requested number of bands...
+        self.assertEqual(nBand, len(lutIndexVals[0]['FILTERNAMES']))
+
+        self.assertFloatsAlmostEqual(i0Std, lutStd[0]['I0STD'], msg='I0Std', rtol=1e-5)
+        self.assertFloatsAlmostEqual(i10Std, lutStd[0]['I10STD'], msg='I10Std', rtol=1e-5)
+
+        indices = fgcmLut.getIndices(np.arange(nBand, dtype=np.int32),
+                                     np.zeros(nBand) + lutStd[0]['PWVSTD'],
+                                     np.zeros(nBand) + lutStd[0]['O3STD'],
+                                     np.zeros(nBand) + np.log(lutStd[0]['TAUSTD']),
+                                     np.zeros(nBand) + lutStd[0]['ALPHASTD'],
+                                     np.zeros(nBand) + 1. / np.cos(np.radians(lutStd[0]['ZENITHSTD'])),
+                                     np.zeros(nBand, dtype=np.int32),
+                                     np.zeros(nBand) + lutStd[0]['PMBSTD'])
+        i0 = fgcmLut.computeI0(np.zeros(nBand) + lutStd[0]['PWVSTD'],
+                               np.zeros(nBand) + lutStd[0]['O3STD'],
+                               np.zeros(nBand) + np.log(lutStd[0]['TAUSTD']),
+                               np.zeros(nBand) + lutStd[0]['ALPHASTD'],
+                               np.zeros(nBand) + 1. / np.cos(np.radians(lutStd[0]['ZENITHSTD'])),
+                               np.zeros(nBand) + lutStd[0]['PMBSTD'],
+                               indices)
+
+        self.assertFloatsAlmostEqual(i0Recon, i0, msg='i0Recon', rtol=1e-5)
+
+        i1 = fgcmLut.computeI1(np.zeros(nBand) + lutStd[0]['PWVSTD'],
+                               np.zeros(nBand) + lutStd[0]['O3STD'],
+                               np.zeros(nBand) + np.log(lutStd[0]['TAUSTD']),
+                               np.zeros(nBand) + lutStd[0]['ALPHASTD'],
+                               np.zeros(nBand) + 1. / np.cos(np.radians(lutStd[0]['ZENITHSTD'])),
+                               np.zeros(nBand) + lutStd[0]['PMBSTD'],
+                               indices)
+
+        self.assertFloatsAlmostEqual(i10Recon, i1 / i0, msg='i10Recon', rtol=1e-5)
+
+    def _runFgcmBuildStars(self, nVisit, nStar, nObs):
         """
         """
 
         if self.logLevel is not None:
             self.otherArgs.extend(['--loglevel', 'fgcmcal=%s'%self.logLevel])
-        args = [self.inputDir, '--output', outputDir,
+        args = [self.inputDir, '--output', self.testDir,
                 '--doraise']
         args.extend(self.otherArgs)
 
-        result = fgcmcal.fgcmBuildStarsTask.parseAndRun(args=args, config=self.config)
+        result = fgcmcal.FgcmBuildStarsTask.parseAndRun(args=args, config=self.config)
         self._checkResult(result)
+
+        butler = dafPersistence.butler.Butler(self.testDir)
+
+        visitCat = butler.get('fgcmVisitCatalog')
+        self.assertEqual(nVisit, len(visitCat))
+
+        starIds = butler.get('fgcmStarIds')
+        self.assertEqual(nStar, len(starIds))
+
+        starObs = butler.get('fgcmStarObservations')
+        self.assertEqual(nObs, len(starObs))
+
+    def _runFgcmFitCycle(self, nZp, nGoodZp):
+        """
+        """
+
+        if self.logLevel is not None:
+            self.otherArgs.extend(['--loglevel', 'fgcmcal=%s'%self.logLevel])
+
+        args = [self.inputDir, '--output', self.testDir,
+                '--doraise']
+        args.extend(self.otherArgs)
+
+        args = [self.inputDir, '--output', self.testDir, '--doraise']
+        result = fgcmcal.FgcmFitCycleTask.parseAndRun(args=args, config=self.config)
+        self._checkResult(result)
+
+        butler = dafPersistence.butler.Butler(self.testDir)
+
+        zps = butler.get('fgcmZeropoints')
+
+        self.assertEqual(nZp, len(zps))
+
+        gd, = np.where(zps['fgcmflag'] == 1)
+        self.assertEqual(nGoodZp, len(gd))
 
     def _checkResult(self, result):
         self.assertNotEqual(result.resultList, [], 'resultList should not be empty')
         self.assertEqual(result.resultList[0].exitStatus, 0)
 
-
+    def tearDown(self):
+        if os.path.exists(self.testDir):
+            shutil.rmtree(self.testDir, True)
 
 
---
+++ tests/test_fgcmcal_hsc.py
@@ -0,0 +1,111 @@
+# See COPYRIGHT file at the top of the source tree.
+
+from __future__ import division, absolute_import, print_function
+
+import inspect
+import unittest
+import os
+import tempfile
+
+import lsst.utils
+import lsst.pex.exceptions
+
+import fgcmcalTestBase
+
+import lsst.fgcmcal as fgcmcal
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+class FgcmcalTestHSC(fgcmcalTestBase.FgcmcalTestBase, lsst.utils.tests.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        try:
+            cls.dataDir = lsst.utils.getPackageDir('testdata_jointcal')
+        except lsst.pex.exceptions.NotFoundError:
+            raise unittest.SkipTest("testdata_jointcal not setup")
+
+    def setUp(self):
+        inputDir = os.path.join(self.dataDir, 'hsc')
+
+        testDir = tempfile.mkdtemp(dir=ROOT, prefix="TestFgcm-")
+
+        self.setUp_base(inputDir=inputDir, testDir=testDir)
+
+        lsst.log.setLevel("HscMapper", lsst.log.FATAL)
+
+    def test_fgcmcalTasks(self):
+
+        # First test making the LUT
+        self.config = fgcmcal.FgcmMakeLutConfig()
+        self.config.filterNames = ['r', 'i']
+        self.config.stdFilterNames = ['r', 'i']
+        self.config.atmosphereTableName = 'fgcm_atm_subaru1_test'
+        self.otherArgs = []
+
+        nBand = 2
+        i0Std = [0.07877351, 0.06464688]
+        i10Std = [-0.00061516, -0.00063434]
+        i0Recon = [0.06897538, 0.05616964]
+        i10Recon = [-6.97094875, 3.69335364]
+
+        self._runFgcmMakeLut(nBand, i0Std, i0Recon, i10Std, i10Recon)
+
+        # Now the star building
+        self.config = fgcmcal.FgcmBuildStarsConfig()
+        self.config.filterToBand = {'r':'r', 'i':'i'}
+        self.config.requiredBands = ['r', 'i']
+        self.config.referenceBand = 'i'
+        self.config.checkAllCcds = True
+        self.otherArgs = []
+
+        nVisit = 11
+        nStar = 472
+        nObs = 5431
+
+        self._runFgcmBuildStars(nVisit, nStar, nObs)
+
+        # And finally the fit cycle
+        self.config = fgcmcal.FgcmFitCycleConfig()
+        self.config.outfileBase = 'TestFgcm'
+        self.config.bands = ['r', 'i']
+        self.config.fitFlag = (1, 1)
+        self.config.filterToBand = {'r':'r', 'i':'i'}
+        self.config.maxIter = 1
+        self.config.nCore = 1
+        self.config.cycleNumber = 0
+        self.config.utBoundary = 0.0
+        self.config.washMjds = (0.0, )
+        self.config.epochMjds = (0.0, 100000.0)
+        self.config.latitude = 19.8256
+        self.config.cameraGain = 3.0
+        self.config.pixelScale = 0.17
+        self.config.expGrayPhotometricCut = (-0.05, -0.05)
+        self.config.expGrayHighCut = (0.2, 0.2)
+        self.config.aperCorrFitNBins = 0
+        self.config.sedFudgeFactors = (1.0, 1.0)
+        self.config.starColorCuts = ('r,i,-0.50,2.25',)
+        self.config.freezeStdAtmosphere = True
+        self.config.precomputeSuperStarInitialCycle = False
+        self.config.minExpPerNight = 1
+        self.config.minStarPerExp = 50
+        self.config.nStarPerRun = 50
+        self.config.nExpPerRun = 2
+        self.config.colorSplitIndices = (0, 1)
+        self.otherArgs = []
+
+        nZp = 1232
+        nGoodZp = 27
+
+        self._runFgcmFitCycle(nZp, nGoodZp)
+
+
+
+class TestMemory(lsst.utils.tests.MemoryTestCase):
+    pass
+
+def setup_module(module):
+    lsst.utils.tests.init()
+
+if __name__ == "__main__":
+    lsst.utils.tests.init()
+    unittest.main()
