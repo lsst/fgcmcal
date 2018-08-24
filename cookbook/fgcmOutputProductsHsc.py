455a0487619c33506ea028a336620a40bc4d3fa6
Eli Rykoff <erykoff@stanford.edu>
2018-08-24 09:27:59 -0700

    Add defaults for cross-calibration in cookbook

--- cookbook/fgcmOutputProductsHsc.py
+++ cookbook/fgcmOutputProductsHsc.py
@@ -2,4 +2,20 @@
 HSC cookbook specific overrides for fgcmOutputProducts
 """
 
-config.cycleNumber = 1
+import os.path
+
+from lsst.utils import getPackageDir
+
+# Last cycle number that was run, preferably with outputStandards == True
+config.cycleNumber = 3
+
+# Reference object info
+config.refObjLoader.ref_dataset_name = 'ps1_pv3_3pi_20170110'
+
+# Photometric calibration information
+config.photoCal.photoCatName = 'ps1'
+hscConfigDir = os.path.join(getPackageDir("obs_subaru"), "config", "hsc")
+config.photoCal.colorterms.load(os.path.join(hscConfigDir, 'colorterms.py'))
+
+
+
