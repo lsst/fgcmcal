d5ea8333436a04b80b13a713fe7dd56c58f3084a
Eli Rykoff <erykoff@stanford.edu>
2020-04-24 11:45:11 -0700

    Reinstall sed terms to match bands

--- cookbook/fgcmFitCycleHscCookbook_cycle00_config.py
+++ cookbook/fgcmFitCycleHscCookbook_cycle00_config.py
@@ -13,3 +13,16 @@ config.maxIterBeforeFinalCycle = 30
 config.epochMjds = [56700., 57420., 57606.]
 # Number of bins to do aperture correction.  Not currently supported in LSST stack.
 config.aperCorrFitNBins = 0
+
+from lsst.fgcmcal import Sedterm, Sedboundaryterm
+config.sedboundaryterms.data = {'gr': Sedboundaryterm(primary='g', secondary='r'),
+                                'ri': Sedboundaryterm(primary='r', secondary='i'),
+                                'iz': Sedboundaryterm(primary='i', secondary='z'),
+                                'zy': Sedboundaryterm(primary='z', secondary='y')}
+config.sedterms.data = {'g': Sedterm(primaryTerm='gr', secondaryTerm='ri', constant=1.6),
+                        'r': Sedterm(primaryTerm='gr', secondaryTerm='ri', constant=0.9),
+                        'i': Sedterm(primaryTerm='ri', secondaryTerm='iz', constant=1.0),
+                        'z': Sedterm(primaryTerm='iz', secondaryTerm='zy', constant=1.0),
+                        'y': Sedterm(primaryTerm='zy', secondaryTerm='iz', constant=0.25,
+                                     extrapolated=True, primaryBand='y', secondaryBand='z',
+                                     tertiaryBand='i')}
