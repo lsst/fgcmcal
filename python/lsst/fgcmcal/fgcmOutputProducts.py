8d91b62b53ce627627f166fa405ac184906e01c7
Eli Rykoff <erykoff@stanford.edu>
2025-01-30 16:55:35 -0800

    Fix floating point initialization of Point2I.

--- python/lsst/fgcmcal/fgcmOutputProducts.py
+++ python/lsst/fgcmcal/fgcmOutputProducts.py
@@ -842,8 +842,8 @@ class FgcmOutputProductsTask(pipeBase.PipelineTask):
         orderPlus1 = int(np.sqrt(coefficients.size))
         pars = np.zeros((orderPlus1, orderPlus1))
 
-        bbox = lsst.geom.Box2I(lsst.geom.Point2I(0.0, 0.0),
-                               lsst.geom.Point2I(*xyMax))
+        bbox = lsst.geom.Box2I(minimum=lsst.geom.Point2I(0, 0),
+                               maximum=lsst.geom.Point2I(*xyMax))
 
         pars[:, :] = (coefficients.reshape(orderPlus1, orderPlus1)
                       * (10.**(offset/-2.5))*scaling)
