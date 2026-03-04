8dbb1badb8c29292b8e38a3089c225df97f3621a
Matthias Wittgen <matthias.michael.wittgen@cern.ch>
2026-03-03 18:31:53 -0800

    Fix for numpy 2.4

--- python/lsst/fgcmcal/fgcmBuildStarsBase.py
+++ python/lsst/fgcmcal/fgcmBuildStarsBase.py
@@ -351,8 +351,8 @@ class FgcmBuildStarsBaseTask(pipeBase.PipelineTask, abc.ABC):
             # Median delta aperture, to be measured from stars
             rec['deltaAper'] = -9999.0
             rec['deltaAperDetector'][:] = -9999.0
-            rec['psfSigma'] = psfSigma
-            rec['psfFwhm'] = psfFwhm
+            rec['psfSigma'] = psfSigma.item()
+            rec['psfFwhm'] = psfFwhm.item()
             # This is keyed by detector.
             rec['psfFwhmDetector'][summary["id"]] = psfFwhms
             rec['skyBackground'] = skyBackground
